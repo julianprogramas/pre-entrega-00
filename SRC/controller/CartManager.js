6669ed6bac8dbc54916dc647c911f3c9f4724cb1
Julian Calvo <juliprogramas@gmail.com>
2023-02-21 21:16:30 -0300

    new

--- SRC/controller/CartManager.js
+++
@@ -1,9 +0,0 @@
-import { promises as fs, existsSync, writeFileSync } from "fs";
-
-class CartManager {
-    constructor(productos) {
-        this.productos = productos
-    }
-}
-
-export default CartManager
